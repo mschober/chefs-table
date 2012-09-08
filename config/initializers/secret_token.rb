90184784e8ea7d7f3d3d9f43afd89ea25b441689
Mike Schober <mschober@cj.com>
2012-09-08 15:04:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefsTable::Application.config.secret_token = 'dd138356460c45a99a02b618f2a1694aba0f1a7abd26086e785c08bc81603aa31a25d3703ae614858eea62ad67b004e0af4ba472b6ea048edee9f16727965d80'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChefsTable::Application.config.session_store :cookie_store, key: '_chefs_table_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChefsTable::Application.config.session_store :active_record_store
